1409e1a4af056a7f4149aa851ea8f9cddef121ef
Jorge Guerra <jguerrag@interior.gob.cl>
2022-08-06 17:52:06 -0400

    Api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ZipCode;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Cache;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function zip_code($code)
+    {
+        $obj = ZipCode::with('federal_entity','municipality','settlements','settlements.settlement_type')
+                    ->find($code);
+        return json_encode($obj,JSON_UNESCAPED_UNICODE);
+    }
 }
